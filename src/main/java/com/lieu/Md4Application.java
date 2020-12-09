d62a3bdc0500a3bbe8be2ee08bc6deabd3577c62
VanLien1503 <hoanglien89nd@gmail.com>
2020-12-09 11:37:29 +0700

    Initial commit

---
+++ src/main/java/com/lieu/Md4Application.java
@@ -0,0 +1,13 @@
+package com.lieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lieu/Md4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
